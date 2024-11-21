545b0ba857e2e9bbeb8b58c874f3a3190a5b582d
JullianeSSousa <jullianecoda@gmail.com>
2024-11-21 15:23:08 -0300

    Adicionando paginas iniciais

---
+++ script.js
@@ -0,0 +1,19 @@
+// O efeito da foto passar para o lado não funciona!!
+
+let currentIndex = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelectorAll('.slides img');
+    const totalSlides = slides.length;
+
+    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
+    
+    const carousel = document.querySelector('.slides');
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+
+    // Muda de slide a cada 3 segundos
+// Muda de slide a cada 3 segundossetInterval(() => {
+   // Muda de slide a cada 3 segundos moveSlide(-1);
+// Muda de slide a cada 3 segundos}, 6000); } // Muda de slide a cada 3 segundo
+}
